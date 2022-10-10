dde2e4d3ea5453d917b1fd45a58441921113d3d7
Juan Sanchez <juan.camilosanchez@hotmail.com>
2022-10-10 12:23:08 -0500

    fix database

--- back/settings.py
+++ back/settings.py
@@ -87,9 +87,7 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
-    'default': {
-        dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite', conn_max_age=600)
-    }
+    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite', conn_max_age=600)
 }
 
 # Password validation
