2de04cc1af492587e3f4445554827920ce097a37
Juan Sanchez <juan.camilosanchez@hotmail.com>
2022-10-10 12:17:24 -0500

    fix database variable

--- back/settings.py
+++ back/settings.py
@@ -88,14 +88,6 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': env('DATABASE_NAME'),
-    #     'USER': env('DATABASE_USER'),
-    #     'PASSWORD': env('DATABASE_PASSWORD'),
-    #     'HOST': env('DATABASE_HOST'),
-    #     'PORT': env('DATABASE_PORT'),
-    # }
     'default': {
         dj_database_url.config()
     }
